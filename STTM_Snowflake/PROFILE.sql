841d08529c94ff0acbf2f177fbec5db434332486
chris-amendola <chris.amendola@gmail.com>
2026-01-16 11:26:52 -0600

    Update PROFILE.sql - BUG

--- STTM_Snowflake/PROFILE.sql
+++ STTM_Snowflake/PROFILE.sql
@@ -1,29 +1,8 @@
-SELECT
-    'SELECT
-        ''' || column_name || ''' AS column_name,
-        ''' || data_type   || ''' AS data_type,
-        ''' || is_nullable || ''' AS is_nullable,
-        COUNT(*) AS rows_scanned,
-        COUNT(' || column_name || ') AS non_null_count,
-        COUNT(*) - COUNT(' || column_name || ') AS null_count,
-        ROUND(COUNT(' || column_name || ') / NULLIF(COUNT(*),0) * 100, 2)
-            AS pct_populated,
-        COUNT(DISTINCT ' || column_name || ') AS distinct_count,
-        ROUND(
-            COUNT(DISTINCT ' || column_name || ') /
-            NULLIF(COUNT(' || column_name || '),0) * 100, 2
-        ) AS pct_distinct_non_null,
-        CAST(MIN(' || column_name || ') AS STRING) AS min_value,
-        CAST(MAX(' || column_name || ') AS STRING) AS max_value
-     FROM STAR_DEV.GGASTRO.STG_ORDER
-     UNION ALL' AS profiling_sql
-FROM STAR.INFORMATION_SCHEMA.COLUMNS
-WHERE table_schema = 'GGASTRO'
-  AND table_name   = 'STG_ORDER'
-ORDER BY ordinal_position;
+USE DATABASE STAR_DEV;
+USE SCHEMA SEMANTIC_MAPPING;
 
-
-CREATE OR REPLACE TEMPORARY TABLE STAR_DEV.SEMANTIC_MAPPING.COLUMN_SUMMARY_RESULTS (
+CREATE OR REPLACE TABLE STAR_DEV.SEMANTIC_MAPPING.COLUMN_SUMMARY_RESULTS (
+        table_name STRING,
         column_name STRING,
         data_type STRING,
         is_nullable STRING,
@@ -37,7 +16,7 @@ CREATE OR REPLACE TEMPORARY TABLE STAR_DEV.SEMANTIC_MAPPING.COLUMN_SUMMARY_RESUL
         max_value STRING
     );
     
-CREATE OR REPLACE PROCEDURE PROFILE(
+CREATE OR REPLACE PROCEDURE PROFILE_COLUMNS(
     DB_NAME STRING, 
     SCHEMA_NAME STRING, 
     TABLE_NAME STRING)
@@ -46,21 +25,21 @@ LANGUAGE SQL
 AS
 DECLARE
     col_cursor CURSOR FOR 
-        SELECT column_name, data_type, is_nullable 
+        SELECT table_name,column_name, data_type, is_nullable 
         FROM STAR_DEV.INFORMATION_SCHEMA.COLUMNS
         WHERE table_name = ? 
           AND table_schema = ?
         ORDER BY ORDINAL_POSITION;
     insert_sql STRING;
-    --full_table_path STRING:= :DB_NAME || '.' || :SCHEMA_NAME || '.' || :TABLE_NAME;
-    full_table_path STRING DEFAULT 'STAR_DEV.GGASTRO.STG_ORDER';
+    full_table_path STRING:= :DB_NAME || '.' || :SCHEMA_NAME || '.' || :TABLE_NAME;
 BEGIN
     -- Loop through each column and execute the summary query
     OPEN col_cursor USING (:TABLE_NAME, :SCHEMA_NAME);
     FOR record IN col_cursor DO
         insert_sql := 'INSERT INTO STAR_DEV.SEMANTIC_MAPPING.COLUMN_SUMMARY_RESULTS ' ||
                       'SELECT 
-                       '''|| record.column_name ||''' as column_name, 
+                      '''|| record.table_name ||''' as table_name, 
+                      '''|| record.column_name ||''' as column_name, 
                       ''' || record.data_type || ''' as data_type,
                       ''' || record.is_nullable ||''' as is_nullable,
                        COUNT(*) as rows_scanned, ' ||
@@ -79,5 +58,44 @@ BEGIN
     RETURN 'Summary completed for table: ' || :full_table_path;
 END;
 
-CALL PROFILE('STAR_DEV', 'GGASTRO', 'STG_ORDER');
+CREATE OR REPLACE PROCEDURE PROFILE_TABLES(SCHEMA_NAME STRING)
+RETURNS STRING
+LANGUAGE JAVASCRIPT
+AS
+$$
+    var result_log = [];
 
+    // Get all table names from INFORMATION_SCHEMA
+    var get_tables_sql = `
+        SELECT TABLE_NAME 
+        FROM STAR_DEV.INFORMATION_SCHEMA.TABLES 
+        WHERE TABLE_SCHEMA = UPPER('${SCHEMA_NAME}')
+          AND TABLE_TYPE = 'BASE TABLE'
+          -- Profile for Incoming data only
+          AND STARTSWITH(TABLE_NAME, 'STG_')
+        ORDER BY TABLE_NAME
+    `;
+    var stmt = snowflake.createStatement({ sqlText: get_tables_sql });
+    var rs = stmt.execute();
+
+    // Loop through each table and call the processing procedure
+    while (rs.next()) {
+        var tbl_name = SCHEMA_NAME + '.' + rs.getColumnValue(1);
+
+        try {
+            var call_sql = `CALL PROFILE_COLUMNS('STAR_DEV', 'GGASTRO','${tbl_name}');`;
+            snowflake.execute({ sqlText: call_sql });
+            result_log.push(`✅ Success: ${tbl_name}`);
+        } catch (err) {
+            result_log.push(`❌ Failed: ${tbl_name} - ${err.message}`);
+        }
+    }
+
+    return result_log.join('\n');
+$$;
+
+CALL PROFILE_TABLES('GGASTRO');
+
+SELECT table_name,COUNT(DISTINCT table_name)  
+FROM STAR_DEV.SEMANTIC_MAPPING.COLUMN_SUMMARY_RESULTS
+GROUP BY table_name;
