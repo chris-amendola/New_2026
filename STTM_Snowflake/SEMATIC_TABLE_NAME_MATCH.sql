53a25436663c2d5565a7b2bb6fac3b81b9315ebe
chris-amendola <chris.amendola@gmail.com>
2026-01-18 13:25:54 -0600

    Update SEMATIC_TABLE_NAME_MATCH.sql

--- STTM_Snowflake/SEMATIC_TABLE_NAME_MATCH.sql
+++ STTM_Snowflake/SEMATIC_TABLE_NAME_MATCH.sql
@@ -1,58 +1,65 @@
 
 USE DATABASE STAR_DEV;
 --EMBED TABLE NAME PLUS COLUMNS
-SELECT 
-    table_name,
-    LISTAGG(column_name, ', ') WITHIN GROUP (ORDER BY ordinal_position) AS column_names
-FROM 
-    STAR_DEV.INFORMATION_SCHEMA.COLUMNS
-WHERE 
-    table_schema = 'GGASTRO'
-GROUP BY 
-    table_name
-ORDER BY 
-    table_name;
-
-
-
-
-
-
-WITH t1 AS (
-    SELECT table_schema, table_name
-    FROM STAR_DEV.INFORMATION_SCHEMA.TABLES
-    WHERE table_schema = 'GGASTRO'
-      AND table_type = 'BASE TABLE'         -- optional but recommended
-),
-t2 AS (
-    SELECT table_schema, table_name
-    FROM STAR_DEV.INFORMATION_SCHEMA.TABLES
-    WHERE table_schema = 'DIM_MODEL'
-      AND table_type = 'BASE TABLE'         -- optional but recommended
-),
-t1_emb AS (
+WITH SOURCE_RAW AS(
     SELECT
-        table_schema AS schema1,
-        table_name   AS table1,
-        SNOWFLAKE.CORTEX.EMBED_TEXT_768('e5-base-v2', table_name::STRING) AS vec1
-    FROM t1
+        table_schema AS tab_schema,
+        table_name,
+        LISTAGG(column_name, ', ') WITHIN GROUP (ORDER BY ordinal_position) AS column_names
+    FROM 
+        STAR_DEV.INFORMATION_SCHEMA.COLUMNS
+    WHERE --WHAT IS TRUE GGASTRO SOURCE TABLE SET?
+          --STG_{*} are point of entry
+          table_schema = 'GGASTRO'
+      AND STARTSWITH(table_name, 'STG_')
+    GROUP BY 
+        table_schema,table_name
 ),
-t2_emb AS (
+TARGET_RAW AS(
+    SELECT 
+        table_schema AS tab_schema,
+        table_name,
+        LISTAGG(column_name, ', ') WITHIN GROUP (ORDER BY ordinal_position) AS column_names
+    FROM 
+        STAR_DEV.INFORMATION_SCHEMA.COLUMNS
+    WHERE 
+        table_schema = 'DIM_MODEL'
+    GROUP BY 
+        table_schema,table_name
+),
+SOURCE_ENC AS(
     SELECT
-        table_schema AS schema2,
-        table_name   AS table2,
-        SNOWFLAKE.CORTEX.EMBED_TEXT_768('e5-base-v2', table_name::STRING) AS vec2
-    FROM t2
+            tab_schema AS schema,
+            table_name,
+            SNOWFLAKE.CORTEX.EMBED_TEXT_768('e5-base-v2', table_name::STRING) AS table_enc,
+            SNOWFLAKE.CORTEX.EMBED_TEXT_768('e5-base-v2', column_names::STRING) AS cols_enc,
+            SNOWFLAKE.CORTEX.EMBED_TEXT_768('e5-base-v2', CONCAT(table_name,',',column_names)) AS tab_cols_enc,
+            SNOWFLAKE.CORTEX.EMBED_TEXT_768('e5-base-v2', CONCAT(column_names,',',table_name)) AS cols_tab_enc
+        FROM SOURCE_RAW
+),
+TARGET_ENC AS(
+    SELECT
+        tab_schema AS schema,
+        table_name,
+        SNOWFLAKE.CORTEX.EMBED_TEXT_768('e5-base-v2', table_name::STRING) AS table_enc,
+        SNOWFLAKE.CORTEX.EMBED_TEXT_768('e5-base-v2', column_names::STRING) AS cols_enc,
+        SNOWFLAKE.CORTEX.EMBED_TEXT_768('e5-base-v2', CONCAT(table_name,',',column_names)) AS tab_cols_enc,
+        SNOWFLAKE.CORTEX.EMBED_TEXT_768('e5-base-v2', CONCAT(column_names,',',table_name)) AS cols_tab_enc
+     FROM TARGET_RAW
 )
 SELECT
-    e1.schema1,
-    e1.table1,
-    e2.schema2,
-    e2.table2,
-    JAROWINKLER_SIMILARITY(e1.table1, e2.table2)/100 as chk1,
-    VECTOR_COSINE_SIMILARITY(e1.vec1, e2.vec2) as ck2,
-    ( (VECTOR_COSINE_SIMILARITY(e1.vec1, e2.vec2)*0.7)+ 
-      (JAROWINKLER_SIMILARITY(e1.table1, e2.table2)/100)*0.3) as score
-FROM t1_emb AS e1
-CROSS JOIN t2_emb AS e2
-ORDER BY table2,score DESC;
+    src.schema AS SOURCE_SCHEMA,
+    src.table_name AS SOURCE_TABLE,
+    tar.schema AS TARGET_SCHEMA,
+    tar.table_name AS TARGET_TABLE,
+    VECTOR_COSINE_SIMILARITY(src.table_enc, tar.table_enc) AS TAB_MATCH,
+    VECTOR_COSINE_SIMILARITY(src.cols_enc, tar.cols_enc) AS COLS_MATCH,
+    VECTOR_COSINE_SIMILARITY(src.tab_cols_enc, tar.tab_cols_enc) AS TAB_COLS_MATCH,
+    VECTOR_COSINE_SIMILARITY(src.cols_tab_enc, tar.cols_tab_enc) AS COLS_TAB_MATCH,
+    (TAB_MATCH+COLS_MATCH+TAB_COLS_MATCH+COLS_TAB_MATCH) AS COMPOSITE_SCORE
+FROM 
+    TARGET_ENC AS tar
+CROSS JOIN 
+    SOURCE_ENC AS src
+--TEMP
+ORDER BY TARGET_TABLE,TAB_COLS_MATCH DESC;
